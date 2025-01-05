ed3b6ee8b496922ff96536843eb46f050c7afa15
unknown <reffa.kaila@gmail.com>
2025-01-06 01:53:35 +0700

    add page kendaraan

--- src/app/dpo/page.js
+++ src/app/dpo/page.js
@@ -12,31 +12,31 @@ export default function DPO() {
             <svg className='hover:animate-pulse cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path></svg>
           </div>
         </div>
-        <div className='rounded-lg border border-[#eaeaea]/50 overflow-hidden m-1'>
+        <div className='rounded-lg ring-1 overflow-hidden m-1'>
           <table className='table-auto w-full'>
             <thead className='bg-[#03040f]'>
               <tr className='text-center'>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>No</td>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>Nama Lengkap</td>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>Reason</td>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>Tanggal Penetapan</td>
-                <td className='text-sm p-2'>Status</td>
+                <td className='text-sm p-2 ring-1'>No</td>
+                <td className='text-sm p-2 ring-1'>Nama Lengkap</td>
+                <td className='text-sm p-2 ring-1'>Reason</td>
+                <td className='text-sm p-2 ring-1'>Tanggal Penetapan</td>
+                <td className='text-sm p-2 ring-1'>Status</td>
               </tr>
             </thead>
             <tbody className='text-center'>
-              <tr className='border-b border-[#eaeaea]/50'>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>1</td>
-                <td className='text-sm p-2 text-left border-r border-[#eaeaea]/50'>Jhon Doe</td>
-                <td className='text-sm p-2 text-left border-r border-[#eaeaea]/50'>Penembakan kepada anggota kepolisian</td>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>21 Mei 2024</td>
-                <td className='text-sm p-2'>On Progress</td>
+              <tr className=''>
+                <td className='text-sm p-2 ring-1'>1</td>
+                <td className='text-sm p-2 text-left ring-1'>Jhon Doe</td>
+                <td className='text-sm p-2 text-left ring-1'>Penembakan kepada anggota kepolisian</td>
+                <td className='text-sm p-2 ring-1'>21 Mei 2024</td>
+                <td className='text-sm p-2 ring-1'>On Progress</td>
               </tr>
-              <tr className='border-b border-[#eaeaea]/50'>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>1</td>
-                <td className='text-sm p-2 text-left border-r border-[#eaeaea]/50'>Jhon Doe</td>
-                <td className='text-sm p-2 text-left border-r border-[#eaeaea]/50'>Penembakan kepada anggota kepolisian</td>
-                <td className='text-sm p-2 border-r border-[#eaeaea]/50'>21 Mei 2024</td>
-                <td className='text-sm p-2'>On Progress</td>
+              <tr className=''>
+                <td className='text-sm p-2 ring-1 '>1</td>
+                <td className='text-sm p-2 text-left ring-1'>Jhon Doe</td>
+                <td className='text-sm p-2 text-left ring-1'>Penembakan kepada anggota kepolisian</td>
+                <td className='text-sm p-2 ring-1'>21 Mei 2024</td>
+                <td className='text-sm p-2 ring-1'>On Progress</td>
               </tr>
             </tbody>
           </table>
